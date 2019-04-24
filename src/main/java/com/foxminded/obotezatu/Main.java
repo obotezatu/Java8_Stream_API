58dadcfc9238639cf8e7f678e85c31eb22557ca8
Oleg Botezatu <olbot@inbox.ru>
2019-04-24 20:22:36 +0300

    draft version 2

--- src/main/java/com/foxminded/obotezatu/Main.java
+++ src/main/java/com/foxminded/obotezatu/Main.java
@@ -1,9 +1,17 @@
 package com.foxminded.obotezatu;
 
+import java.io.IOException;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
+		try {
+			QualificationRace.readTime();
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
 
 	}
 
--- src/main/java/com/foxminded/obotezatu/QualificationRace.java
+++ src/main/java/com/foxminded/obotezatu/QualificationRace.java
@@ -1,5 +1,26 @@
 package com.foxminded.obotezatu;
 
-public class QualificationRace {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Instant;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class QualificationRace {
+	
+	public static void readTime() throws IOException {
+		
+		List<Racer> racerList = Files.lines(Paths.get("./src/main/resources/start.log")).map(QualificationRace::transform).collect(Collectors.toList());
+	}
+	
+	private static  Racer transform(String line) {
+		Racer racer = new Racer();
+		
+		racer.setId(line.substring(0, 3));
+		String time = line.substring(3).replace('_', 'T').concat("Z");
+		Instant instant = Instant.parse(time);
+		return racer;
+	}
 }
