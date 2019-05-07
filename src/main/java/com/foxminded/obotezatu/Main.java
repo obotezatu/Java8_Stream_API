cbab05d37869b82c105d6ae176373d3913483d71
Oleg Botezatu <olbot@inbox.ru>
2019-05-07 18:56:35 +0300

    draft version ?

--- src/main/java/com/foxminded/obotezatu/Main.java
+++ src/main/java/com/foxminded/obotezatu/Main.java
@@ -5,6 +5,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 import java.util.Collections;
 import java.util.List;
 
@@ -36,15 +37,20 @@ public class Main {
 	}
 
 	private static String getLapTime(Racer racer) {
-		final long MILLIS_PER_SEC = 1000;
+		/*final long MILLIS_PER_SEC = 1000;
 		final long MINUTES_PER_HOUR = 60;
 		final long MILLIS_PER_MINUTE = 60 * MILLIS_PER_SEC;
-		final long MILLIS_PER_HOUR = MILLIS_PER_MINUTE * MINUTES_PER_HOUR * MILLIS_PER_SEC;
+		final long MILLIS_PER_HOUR = MILLIS_PER_MINUTE * MINUTES_PER_HOUR * MILLIS_PER_SEC;*/
 
-		long duration = Duration.between(racer.getStartLap(), racer.getEndLap()).toMillis();
-		long minutes = ((duration % MILLIS_PER_HOUR) / MILLIS_PER_MINUTE);
+		//long duration = Duration.between(racer.getStartLap(), racer.getEndLap()).toMillis();
+		Duration duration = Duration.between(racer.getStartLap(), racer.getEndLap());
+		long minutes = duration.toMinutes();
+		long secs = duration.minusMinutes(minutes).getSeconds();
+		long millis = duration.minusMinutes(minutes).minusSeconds(secs).toMillis();
+		//List <TemporalUnit> tempUnit = dur.getUnits();
+		/*long minutes = ((duration % MILLIS_PER_HOUR) / MILLIS_PER_MINUTE);
 		long secs = (duration % MILLIS_PER_MINUTE) / MILLIS_PER_SEC;
-		long millis = (duration % MILLIS_PER_SEC);
+		long millis = (duration % MILLIS_PER_SEC);*/
 		return String.format("%02d:%02d.%03d", minutes, secs, millis);
 	}
 
