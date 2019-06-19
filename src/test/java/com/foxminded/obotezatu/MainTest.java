f148b389f795f3e136a1216c617ee6ed0a259120
md_obotezatu <md_obotezatu@MD2CFDA16E7FDF.alliedtesting.com>
2019-06-19 20:22:24 +0300

    draft version 14

---
+++ src/test/java/com/foxminded/obotezatu/MainTest.java
@@ -0,0 +1,34 @@
+package com.foxminded.obotezatu;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+
+	private RacerRepository racerRepository;
+	private List<Racer> racers;
+
+	@Before
+	public void setUp() {
+		racerRepository = new RacerRepository();
+		racers = racerRepository.readRacers();
+	}
+
+	@Test
+	public void testRacersSort() {
+		Collections.sort(racers, (currentRacer, nextRacer) -> {
+			return currentRacer.getLapTime().getLapDuration().compareTo(nextRacer.getLapTime().getLapDuration());
+		});
+		String expectedFirst = "Sebastian Vettel";
+		String expectedLast = "Kevin Magnussen";
+		String actualFirst = racers.get(0).getName();
+		String actualLast = racers.get(racers.size() - 1).getName();
+
+		assertEquals(expectedFirst, actualFirst);
+		assertEquals(expectedLast, actualLast);
+	}
+}
--- src/test/java/com/foxminded/obotezatu/RacerRepositoryTest.java
+++ src/test/java/com/foxminded/obotezatu/RacerRepositoryTest.java
@@ -1,12 +1,49 @@
 package com.foxminded.obotezatu;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import static org.junit.Assert.assertEquals;
+import org.junit.Before;
+import org.junit.Test;
 
 public class RacerRepositoryTest {
 
-	public static void main(String[] args) {
-		RacerRepository rcer = new RacerRepository();
-		List<Racer> listRacer = rcer.readRacers();
-		listRacer.stream().forEach(racer->System.out.println(racer.getName()));
+	private RacerRepository racerRepository;
+	private List<Racer> racers;
+
+	@Before
+	public void setUp() {
+		racerRepository = new RacerRepository();
+		racers = racerRepository.readRacers();
 	}
+
+	@Test
+	public void testReadRacersListSize() {
+		Path ABBREVIATION_FILE_PATH;
+		try {
+			ABBREVIATION_FILE_PATH = Paths.get(getClass().getClassLoader().getResource("abbreviations.txt").toURI());
+			long expected = Files.lines(ABBREVIATION_FILE_PATH).count();
+
+			assertEquals(expected, racers.size());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (URISyntaxException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	@Test(expected = NullPointerException.class)
+	public void testReadRacersWrongFileName() {
+		try {
+			Path ABBREVIATION_FILE_PATH = Paths.get(getClass().getClassLoader().getResource("abbr.txt").toURI());
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
