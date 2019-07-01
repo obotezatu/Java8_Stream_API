5f91f354c1597fc8bab5f046dc74f2c8eeb96c45
Oleg Botezatu <obotezatu@gmail.com>
2019-07-01 23:29:12 +0300

    draft version 26

--- src/main/java/com/foxminded/obotezatu/RacerFormatter.java
+++ src/main/java/com/foxminded/obotezatu/RacerFormatter.java
@@ -7,15 +7,16 @@ import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class RacerFormatter {
-	
+
 	public String format(List<Racer> racers, int limit) {
 		StringBuilder formattedResult = new StringBuilder();
 		AtomicInteger count = new AtomicInteger(1);
-		racers.stream().limit(limit).forEach(racer ->formattedResult.append(formatOutput(racer,count)));
+		racers.stream().limit(limit).forEach(racer -> formattedResult.append(formatOutput(racer, count)));
 		if (count.get() >= limit) {
-			formattedResult.append("--------------------------------------------------------------").append(lineSeparator());
+			formattedResult.append("--------------------------------------------------------------")
+					.append(lineSeparator());
 		}
-		racers.stream().skip(limit).forEach(racer ->formattedResult.append(formatOutput(racer,count)));
+		racers.stream().skip(limit).forEach(racer -> formattedResult.append(formatOutput(racer, count)));
 		return formattedResult.toString();
 	}
 
@@ -26,8 +27,9 @@ public class RacerFormatter {
 		long millis = duration.minusMinutes(minutes).minusSeconds(seconds).toMillis();
 		return String.format("%02d:%02d.%03d", minutes, seconds, millis);
 	}
-	
-	private  String formatOutput(Racer racer, AtomicInteger count) {
-		return String.format("%3d. %-17s | %-25s | %s%s",count.getAndIncrement(), racer.getName(), racer.getTeam(), getFormattedLapTime(racer),lineSeparator());
+
+	private String formatOutput(Racer racer, AtomicInteger count) {
+		return String.format("%3d. %-17s | %-25s | %s%s", count.getAndIncrement(), racer.getName(), racer.getTeam(),
+				getFormattedLapTime(racer), lineSeparator());
 	}
 }
